7176bc0ad8174b224a1c91c5af45874619b47add
PeterKow <peter.kowalczyk@aurity.co>
2015-10-21 00:24:25 +0100

    creating separate component for hotkey for more declarative way

--- training/00-hello-world/exercise.js
+++ training/00-hello-world/exercise.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Mario from './lib/components/mario.js'
+import HotKey from './lib/components/hotkey.js'
 
 export default class HelloWorld extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      x: 100,
+      y: 100
+    }
+  }
+
+  right(){
+    console.log('right')
+    this.setState({ x: this.state.x + 1})
+  }
+
   render() {
     return <div>Hello
-              <Mario startingPosition='left' position={{ x: 100, y: 100 }}/>
+              <HotKey right={::this.right}>
+                <Mario startingPosition='left' position={{ x: this.state.x, y: this.state.y }}/>
+              </HotKey>
            </div>
   }
 }
